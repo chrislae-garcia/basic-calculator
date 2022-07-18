a85aa2402c21d00bd77c435397d27d61a09aa96d
Christian Laettner Garcia <chrislae.garcia@gmail.com>
2022-07-18 15:21:29 +0800

    Add input validation to display digits and decimal

--- script.js
+++ script.js
@@ -32,16 +32,17 @@ function operate(operator, a, b) {
 }
 
 let displayValue = '';
+const validInput = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
 
 function displayToScreen(e) {
   if (!e.target.type) return;
   let input = e.target.textContent;
 
   switch (true) {
-    case parseInt(input) >= 0:
-    case parseInt(input) <= 9:
+    case validInput.includes(input):
       displayValue += input;
       spanOutput.textContent = displayValue;
+      break;
   }
 }
 
